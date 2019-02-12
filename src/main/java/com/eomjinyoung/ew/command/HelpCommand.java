f68c7cdd7884db1eb1063f6a7d4b6f1019aacf33
Jinyoung Eom <jinyoung.eom@gmail.com>
2019-02-13 06:51:51 +0900

    그룹목록 출력할 때 단어 개수 출력

--- src/main/java/com/eomjinyoung/ew/command/HelpCommand.java
+++ src/main/java/com/eomjinyoung/ew/command/HelpCommand.java
@@ -7,7 +7,7 @@ public class HelpCommand implements Command {
 
   @Override
   public void execute() {
-    System.out.println("영단어 암기 훈련 1.0.1 - 2019.02.12");
+    System.out.println("영단어 암기 훈련 1.0.2 - 2019.02.13");
     System.out.println("[사용할 수 있는 명령]");
     System.out.println("  test - 단어 연습");
     System.out.println("  add - 단어 추가");
